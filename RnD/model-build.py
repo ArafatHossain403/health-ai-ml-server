b5482a562692c857549c56110a18c935b8962bd9
Dipankar <dipankarsaha1059@gmail.com>
2023-12-27 17:18:57 +0600

    Pedigree feature/input filed removed

--- RnD/model-build.py
+++ RnD/model-build.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import os
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 current_dir = os.getcwd()
@@ -14,12 +15,13 @@ model_export_path = "web-server/diabetes-model.pkl"
 if current_dir == "RnD":
     model_export_path = "../" + model_export_path
 
-daibetes = pd.read_csv(csv_file_path)
+dataset = pd.read_csv(csv_file_path)
 
-train_X = daibetes[['Pregnancies','Glucose','BloodPressure','SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']] # taking the training data features
-train_y=daibetes.Outcome
+train_X = dataset[['Pregnancies','Glucose','BloodPressure','SkinThickness', 'Insulin', 'BMI', 'Age']] # taking the training data features
+train_y=dataset.Outcome
 
-model = LogisticRegression(random_state=0, max_iter=1000)
+# model = LogisticRegression(random_state=1, max_iter=1000)
+model=RandomForestClassifier(random_state=1, max_depth=5)
 model.fit(train_X,train_y)
 
 pickle.dump(model, open(model_export_path, 'wb'))
